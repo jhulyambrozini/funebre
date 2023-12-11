67860de59c976fa43c79ed43bdf9b9a09f9cc286
jhuly ambrozini <jhulyamfebr@gmial.com>
2023-12-11 19:32:46 -0300

    feat: criando carosssel de planos

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,3 +1,4 @@
+////////////////////////////-----------MENU MOBILE------//////////////////////////////////////////
 const menuMobileButton: HTMLButtonElement | null = document.querySelector(
   '.menu-mobile-button'
 );
@@ -40,3 +41,38 @@ menuMobileLinks.forEach(item => {
   item.addEventListener('touchstart', closeMenuList);
   item.addEventListener('click', closeMenuList);
 });
+
+////////////////////////////-----------CAROULSE PLANS------//////////////////////////////////////////
+const nextButton = document.querySelector('#next-button');
+const prevButton = document.querySelector('#prev-button');
+const carouselItems: NodeListOf<HTMLDivElement> =
+  document.querySelectorAll('.carousel-item');
+const carouselContainer: HTMLDivElement | null = document.querySelector(
+  '.plans__carousel-mobile__items'
+);
+let currentIndex = 0;
+
+nextButton?.addEventListener('click', () => {
+  if (currentIndex === 1) return;
+  currentIndex = (currentIndex + 1) % carouselItems.length;
+  updateSlider();
+});
+
+prevButton?.addEventListener('click', () => {
+  if (currentIndex === 2) return;
+  currentIndex =
+    (currentIndex - 1 + carouselItems.length) % carouselItems.length;
+  updateSlider();
+});
+
+function updateSlider() {
+  if (carouselContainer) {
+    if (currentIndex === 2) {
+      let newPosition = 230;
+      carouselContainer.style.transform = `translateX(${newPosition}px)`;
+    } else {
+      let newPosition = -currentIndex * 230;
+      carouselContainer.style.transform = `translateX(${newPosition}px)`;
+    }
+  }
+}
